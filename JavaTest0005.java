49297b171e16bf852d9412db3fcd27fe40b95680
robinpatel-tech <robinpatel.in@gmail.com>
2026-03-14 11:49:40 +0530

    Add JAVA TEST 0005 solution

---
+++ JavaTest0005.java
@@ -0,0 +1,20 @@
+class Pattern
+{
+   void PrintPattern()
+   {
+    int num = 1;
+        for (int i = 1; i <= 5; i++) {        
+            for (int j = 1; j <= i; j++) {    
+                System.out.print(num + " ");
+                num++;
+                }
+                System.out.println();             
+                }
+   }
+}
+public class JavaTest0005 {
+    public static void main(String[] args) {
+     Pattern p1 = new Pattern();
+     p1.PrintPattern();
+    }
+}
